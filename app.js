c322191627223463e7be2aa0db0624e785822ecd
joseph g <robhaywood12@gmail.com>
2018-06-20 21:22:08 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+
+var app = express();
+
+
+
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The gamer exchange server has started!");
+});
